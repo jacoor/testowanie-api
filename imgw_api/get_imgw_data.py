2e0eee76a1949573fac4c5f5462557b438a0f672
mkrzeminski <mkrzeminski@imgw.pl>
2023-06-19 00:59:38 +0200

    TEST BY NAME AND OOP MANAGER
    Created manager helps manage application (OOP), added new test and made
    little refactor

--- imgw_api/get_imgw_data.py
+++ imgw_api/get_imgw_data.py
@@ -1,42 +1,117 @@
 import requests
 import pandas as pd
+from unidecode import unidecode
 
 
-def get_all_current_data():
-    """
-    Get current data of all stations
-    :return:
-    DataFrame
-    """
-    response = requests.get(
-        "https://danepubliczne.imgw.pl/api/data/synop"
-    ).json()
+class WeatherImgw:
+    URL = "https://danepubliczne.imgw.pl/api/data/synop"
 
-    if response:
-        df_data = pd.DataFrame(response)
-        return df_data
+    @classmethod
+    def get_all_current_data(cls):
+        """
+        Get current data of all stations
+        :return:
+        DataFrame
+        """
+        response = requests.get(cls.URL).json()
 
-    return "The newest data didn't come jet. Wait 5 min and try again"
+        if response:
+            df_data = pd.DataFrame(response)
+            return df_data
+
+        return "The newest data didn't come jet. Wait 5 min and try again"
+
+    @classmethod
+    def get_current_weather_data_by_station_id(cls, station_id):
+        """
+        Get weather data by index
+        :return:
+        DataFrame
+        """
+        response = requests.get(
+            f"{cls.URL}/id/{station_id}"
+        ).json()
+
+        if not response.get("status"):
+            return response.get("message")
+        print(response.get("status"))
+        city_df = pd.DataFrame(response, index=[response.get("id_stacji")])
+        return city_df
+
+    @classmethod
+    def get_current_weather_data_by_name(cls, station_name):
+        """
+        Get weather data by station name
+        :return:
+        DataFrame
+        """
+        response = requests.get(f"{cls.URL}/station/{station_name}").json()
+
+        if not response.get("status"):
+            return response.get("message")
+
+        city_df = pd.DataFrame(response, index=[response.get("id_stacji")])
+        return city_df
 
 
-def get_station_current_weather_data(station_id):
-    """
-    Get weather data by index
-    :return:
-    DataFrame
-    """
-    response = requests.get(
-        f"https://danepubliczne.imgw.pl/api/data/synop/id/{station_id}"
-    ).json()
+class WeatherManager(WeatherImgw):
+    def __init__(self, name, your_choice=None, action=True):
+        self.name = name
+        self.choice = your_choice
+        self.action = action
 
-    if response.get("status"):
-        return response.get("message")
+    def manage_your_choice(self):
+        """
+        The Manager helps us to shuffle our weather methods - it depends on
+        what the user chooses
+        :return:
+        Print of right result as Data Frame
+        """
+        if self.choice == "1":
+            print(self.get_all_current_data())
+            self.choice = input("Co teraz wybierasz?\n")
 
-    city_df = pd.DataFrame(response, index=[response.get("id_stacji")])
-    return city_df
+        elif self.choice == "2":
+            station_id = input("Podaj numer stacji: ")
+            print(self.get_current_weather_data_by_station_id(station_id))
+            self.choice = input("\nCo teraz wybierasz?\n")
+        elif self.choice == "3":
+            station_name = input("Podaj numer nazwę stacji: ")
+            station_name = station_name.lower().replace(" ", "")
+            print(
+                self.get_current_weather_data_by_name(unidecode(station_name))
+            )
+            self.choice = input("\nCo teraz wybierasz?\n")
+        elif self.choice == "0":
+            self.action = False
+            return self.action
+        else:
+            print("\nWprowadź poprawną wartość z listy")
+            self.choice = input("\nCo wybierasz?\n")
 
 
 if __name__ == "__main__":
-    print(get_all_current_data())
-    city = get_station_current_weather_data(12500)
-    print(city)
+    guy = input("Jak masz na imię: ")
+    weather_guy = WeatherManager(guy)
+
+    print(
+        f"Witaj {weather_guy.name}, w aplikacji dostarczającej aktualnych "
+        f"danych pogodowych."
+    )
+    print(
+        "Poniżej masz kilka opcji do wyboru\n"
+        "1 - pobierz aktualną pogodę dla całej Polski\n"
+        "2 - pobierz aktualną pogodę dla miasta w Polsce podając jego id\n"
+        "3 - pobierz aktualną pogodę dla miasta w Polsce podając jego nazwę\n"
+        "0 - zakończ działanie programu\n"
+    )
+
+    choice = input("Co wybierasz?\n")
+    weather_guy.choice = choice
+
+    while weather_guy.action:
+        weather_guy.manage_your_choice()
+
+
+
+
--- imgw_api/tests/test_get_imgw_api_data.py
+++ imgw_api/tests/test_get_imgw_api_data.py
@@ -13,32 +13,31 @@ def test_get_all_current_data(fake_weather):
     response_keys = pd.DataFrame(response).keys()
 
     assert list(response_keys) == list(fake_keys)
-
-
-@mark.imgw
-def test_get_all_current_data_by_number(fake_weather):
-    response = requests.get(
-        "https://danepubliczne.imgw.pl/api/data/synop"
-    ).json()
-
     assert len(response) == len(fake_weather)
 
 
 @mark.imgw
-def test_get_station_current_weather_data(fake_jelenia_weather):
+def test_get_current_weather_data_by_station_id(fake_jelenia_weather):
     response = requests.get(
         "https://danepubliczne.imgw.pl/api/data/synop/id/12500"
     ).json()
-    df = pd.DataFrame(response, index=[response.get("id_stacji")])
-    station = df.at["12500", "stacja"]
 
-    assert station == fake_jelenia_weather.get("stacja")
+    assert response.get("stacja") == fake_jelenia_weather.get("stacja")
 
 
 @mark.imgw
-def test_get_station_current_weather_by_bad_number():
+def test_get_current_weather__data_by_bad_station_id():
     response = requests.get(
         "https://danepubliczne.imgw.pl/api/data/synop/id/125e00"
     ).json()
 
     assert response.get("status") is False
+
+
+@mark.imgw
+def test_get_current_weather_data_by_name(fake_jelenia_weather):
+    response = requests.get(
+        f"https://danepubliczne.imgw.pl/api/data/synop/station/jeleniagora"
+    ).json()
+
+    assert response.get("stacja") == fake_jelenia_weather.get("stacja")
