ff91950dbc255d79873c868df08d8256a5df0c47
mkrzeminski <mkrzeminski@imgw.pl>
2023-06-20 00:07:14 +0200

    REFACTORING
    Refactor my existing code. Simple changes.

--- imgw_api/get_imgw_data.py
+++ imgw_api/get_imgw_data.py
@@ -11,53 +11,55 @@ class WeatherImgw:
         """
         Get current data of all stations
         :return:
-        DataFrame
+        DataFrame if exist
         """
         response = requests.get(cls.URL).json()
 
         if response:
             df_data = pd.DataFrame(response)
-            return df_data
+            return print(df_data)
 
-        return "The newest data didn't come jet. Wait 5 min and try again"
+        print("The newest data didn't come jet. Wait 5 min and try again")
 
     @classmethod
     def get_current_weather_data_by_station_id(cls, station_id):
         """
         Get weather data by index
         :return:
-        DataFrame
+        Series if exist
         """
         response = requests.get(
             f"{cls.URL}/id/{station_id}"
         ).json()
 
-        if not response.get("status"):
-            return response.get("message")
-        print(response.get("status"))
-        city_df = pd.DataFrame(response, index=[response.get("id_stacji")])
-        return city_df
+        if response.get("status") is False:
+            return print(response.get("message"))
+
+        city_df = pd.Series(response)
+
+        return print(city_df)
 
     @classmethod
     def get_current_weather_data_by_name(cls, station_name):
         """
         Get weather data by station name
         :return:
-        DataFrame
+        Series if exist
         """
         response = requests.get(f"{cls.URL}/station/{station_name}").json()
 
-        if not response.get("status"):
-            return response.get("message")
+        if response.get("status") is False:
+            return print(response.get("message"))
 
-        city_df = pd.DataFrame(response, index=[response.get("id_stacji")])
-        return city_df
+        city_df = pd.Series(response)
+
+        return print(city_df)
 
 
 class WeatherManager(WeatherImgw):
-    def __init__(self, name, your_choice=None, action=True):
+    def __init__(self, name, choice_number=None, action=True):
         self.name = name
-        self.choice = your_choice
+        self.choice_number = choice_number
         self.action = action
 
     def manage_your_choice(self):
@@ -65,29 +67,30 @@ class WeatherManager(WeatherImgw):
         The Manager helps us to shuffle our weather methods - it depends on
         what the user chooses
         :return:
-        Print of right result as Data Frame
+        Print of right result as Data Frame or DataSeries
         """
-        if self.choice == "1":
-            print(self.get_all_current_data())
-            self.choice = input("Co teraz wybierasz?\n")
+        if self.choice_number == "1":
+            self.get_all_current_data()
+            self.choice_number = input("Co teraz wybierasz?\n")
 
-        elif self.choice == "2":
+        elif self.choice_number == "2":
             station_id = input("Podaj numer stacji: ")
-            print(self.get_current_weather_data_by_station_id(station_id))
-            self.choice = input("\nCo teraz wybierasz?\n")
-        elif self.choice == "3":
+            self.get_current_weather_data_by_station_id(station_id)
+            self.choice_number = input("\nCo teraz wybierasz?\n")
+
+        elif self.choice_number == "3":
             station_name = input("Podaj numer nazwę stacji: ")
             station_name = station_name.lower().replace(" ", "")
-            print(
-                self.get_current_weather_data_by_name(unidecode(station_name))
-            )
-            self.choice = input("\nCo teraz wybierasz?\n")
-        elif self.choice == "0":
+            self.get_current_weather_data_by_name(unidecode(station_name))
+            self.choice_number = input("\nCo teraz wybierasz?\n")
+
+        elif self.choice_number == "0":
             self.action = False
             return self.action
+
         else:
             print("\nWprowadź poprawną wartość z listy")
-            self.choice = input("\nCo wybierasz?\n")
+            self.choice_number = input("\nCo wybierasz?\n")
 
 
 if __name__ == "__main__":
@@ -107,7 +110,7 @@ if __name__ == "__main__":
     )
 
     choice = input("Co wybierasz?\n")
-    weather_guy.choice = choice
+    weather_guy.choice_number = choice
 
     while weather_guy.action:
         weather_guy.manage_your_choice()
